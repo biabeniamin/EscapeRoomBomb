e0d0c433d86c4ceece093e063f3d7a4f9c8ea245
biabeniamin <biabeniamin@gmail.com>
2018-07-24 19:25:05 +0300

    configure pin at boot

--- EscapeRoomBomb.ino
+++ EscapeRoomBomb.ino
@@ -20,7 +20,7 @@ Display display(A0, A2, A1);
 BombTimer *bombTimer;
 
 BYTE insertedKeys[4];
-BYTE correctPin[] = {1, 2, 3, 4};
+BYTE correctPin[] = {9, 9, 9, 9};
 BYTE insertedKeysCount = 0;
 
 BYTE initialTime[8];
@@ -42,7 +42,7 @@ void setup() {
   bombTimer->TurnOn();
 
   display.Write(0xFFFFFFFF);
-  
+
 }
 
 void loop() {
@@ -75,39 +75,65 @@ void loop() {
 
 void SetupMode(DWORD Key)
 {
+  Serial.print("Setup ");
+  Serial.println(Key);
   DWORD number;
 
   number = 0;
-
-  initialTime[initialTimeCount++] = Key;
-  for (int i = 0; i < initialTimeCount; i++)
+  if (1 == setupMode)
   {
-    number *= 10;
-    number += initialTime[i];
+    initialTime[initialTimeCount++] = Key;
+    for (int i = 0; i < initialTimeCount; i++)
+    {
+      number *= 10;
+      number += initialTime[i];
+    }
+
+    if (4 < initialTimeCount)
+    {
+      DWORD seconds;
+
+      seconds = 0;
+
+      seconds = initialTime[3] * 10 + initialTime[4];
+      seconds += (initialTime[1] * 10 + initialTime[2]) * 60;
+      seconds += (initialTime[0]) * 60 * 60;
+
+      bombTimer->GetBombTime()->SetTime(seconds);
+      setupMode = 2;
+      insertedKeysCount = 0;
+      number = 9999;
+      Serial.println("End setup 1");
+    }
+  }
+  else if (2 == setupMode)
+  {
+    correctPin[insertedKeysCount++] = Key;
+    for (int i = 0; i < 4; i++)
+    {
+      number *= 10;
+      number += correctPin[i];
+
+      Serial.print("correct pin ");
+      Serial.println(correctPin[i]);
+    }
+
+    if (3 < insertedKeysCount)
+    {
+      setupMode = 0;
+      insertedKeysCount = 0;
+      bombTimer->StartBombTimer();
+    }
   }
 
+  Serial.println(number);
   display.WriteInDecimal(number);
-
-  if (4 < initialTimeCount)
-  {
-    DWORD seconds;
-
-    seconds = 0;
-
-    seconds = initialTime[3] * 10 + initialTime[4];
-    seconds += (initialTime[1] * 10 + initialTime[2]) * 60;
-    seconds += (initialTime[0]) * 60 * 60;
-
-    bombTimer->GetBombTime()->SetTime(seconds);
-    bombTimer->StartBombTimer();
-    setupMode = 0;
-  }
 
 }
 
 void KeyPressed(BYTE Key)
 {
-  if(PIN_ERASE_INTERVAL < (bombTimer->GetUpTime()->GetTotalMiliSeconds() - lastKeyPress))
+  if (PIN_ERASE_INTERVAL < (bombTimer->GetUpTime()->GetTotalMiliSeconds() - lastKeyPress))
   {
     insertedKeysCount = 0;
   }
